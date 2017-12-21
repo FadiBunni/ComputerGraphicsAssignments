ca999190f7c4a67dbe426137016c48874d013e2d
Zaggralez <jacob@pjetursson.com>
2017-12-21 14:36:01 +0100

    bug

--- assignments/W1/P1/js/sketch.js
+++ assignments/W1/P1/js/sketch.js
@@ -9,6 +9,7 @@ var init = function() {
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 0.3921, 0.5843, 0.9294, 1.0 );
+    gl.clear( gl.COLOR_BUFFER_BIT );
 };
 
 init();
